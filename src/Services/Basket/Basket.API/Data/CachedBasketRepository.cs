bb7ccfe777ea3aa3fda346ebda3ff9304886d08c
Rafek <rafeknaim@gamil.com>
2025-03-10 00:48:45 +0200

    Updated

--- src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -11,7 +11,7 @@ public class CachedBasketRepository(IBasketRepository repository,IDistributedCac
         var cachedBasket = await cache.GetStringAsync(userName, cancellationToken);
 
         if (!string.IsNullOrEmpty(cachedBasket))
-         return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket);
+         return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
 
         var basket = await repository.GetBasketAsync(userName, cancellationToken);
 
