87e9b093f43e96ace659636ca4a0ca4cc022c125
Rafek <rafeknaim@gamil.com>
2025-03-19 03:36:46 +0200

    AddCoomandOrders

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,19 @@
+﻿
+
+using FluentValidation;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+ public record DeleteOrderCommand(Guid Id)
+     : ICommand<DeleteOrderResult>;
+
+    public record DeleteOrderResult(bool isSuccess);
+
+    public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand> {
+
+        public DeleteOrderCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("OrderId is required");
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.UpdateOrder;
+
+public record UpdateOrderCommand(OrderDto Order)
+    :ICommand<UpdateOrderResult>;
+
+public record UpdateOrderResult(bool Succeeded);
+
+public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+{
+    public UpdateOrderCommandValidator()
+    {
+        RuleFor(x => x.Order.Id).NotEmpty().WithMessage("OrderId is required");
+        RuleFor(x => x.Order.OrderName).NotNull().WithMessage("Order Name is required");
+        RuleFor(x => x.Order.CustomerId).NotEmpty().WithMessage("CustomerId is required");
+    }
+}
