40ddcc32f63b2ebaa69a071ccf8938195a8097b7
Rafek <rafeknaim@gamil.com>
2025-04-01 02:06:42 +0200

    AddClientWebApp

---
+++ src/WebApps/Shopping.Web/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Shopping.Web.Models.Catalog;
+
+global using Refit;
+global using Shopping.Web.Services;
+
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+
